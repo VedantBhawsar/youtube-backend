32fd0550af715eb8075118b017ca7d44c386d8a8
Vedant Shankar Bhavsar <vedantbhavsar.a10@gmail.com>
2023-11-19 11:10:24 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-video_id = db.Column(db.String, primary_key=True, unique=True)
-    title = db.Column(db.String(50), nullable=False)
-    thumbnail = db.Column(JSON, nullable=False)
-    quality = db.Column(JSON, nullable=False)
